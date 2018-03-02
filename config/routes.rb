76f452b7901cf81df7c3dfdb98b752b0d76c9059
Florine Lequette <florine.lequette@gmail.com>
2018-03-02 17:59:47 +0100

    add deal model

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   resources :deals
 
-
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
