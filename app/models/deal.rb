d9cdd8d2427f48e96d00eb577ce3df71a817158f
Florine Lequette <florine.lequette@gmail.com>
2018-03-02 18:13:59 +0100

    add validation

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,5 +1,7 @@
 class Deal < ApplicationRecord
-  validates :name, presence:true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
   validates :photo, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
 
