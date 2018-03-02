aed3c1bdcb225f203163bd656016a69ca350e957
Florine Lequette <florine.lequette@gmail.com>
2018-03-02 18:07:42 +0100

    add model deal

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,2 +1,6 @@
 class Deal < ApplicationRecord
+  validates :name, presence:true
+  validates :photo, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302165429) do
+ActiveRecord::Schema.define(version: 20180302170317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180302165429) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "users", force: :cascade do |t|
