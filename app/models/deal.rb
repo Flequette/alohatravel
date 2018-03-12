5121a13a7af84d555892f36a68b9e24ad7bc608c
Florine Lequette <florine.lequette@gmail.com>
2018-03-12 16:01:36 +0100

    model profile deal & bokking ok

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,6 +1,10 @@
 class Deal < ApplicationRecord
+  belongs_to :profile
+  has_many :profiles, through: :bookings
+  has_many :bookings, dependent: :destroy
+
   validates :name, presence: true
-  validates :description, presence: true
+  validates :description, presence: true, length: { maximum:2500 }
   validates :location, presence: true
   validates :photo, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
---
+++ app/models/profile.rb
@@ -0,0 +1,11 @@
+class Profile < ApplicationRecord
+  belongs_to :user
+  has_many :deals, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age, presence: true
+  validates :location, presence: true
+  validates :phone_number, presence: true
+end
